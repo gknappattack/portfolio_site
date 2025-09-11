2dd0346095e12dc3e49c897c45bfb7405b1d64cc
gknappattack <gregory.c.knapp@gmail.com>
2025-09-11 01:13:29 -0400

    Update home page to use components and create landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,14 @@
-import Image from "next/image";
+import NavBar from '@/components/navbar';
+import Introduction from '@/components/introduction';
+import Cover from "@/components/cover"
 
 export default function Home() {
   return (
-  <div>
-    <div className="p-4">
-      <header className="py-4">
-        <div className="mx-auto max-w-7xl bg-[#FFD6FF] px-4 py-3 sm:px-6 lg:px-8">
-          <nav className="relative z-50 flex justify-between">
-            <div className="flex items-center md:gap-x-12">
-              <div className="hidden md:flex md:gap-x- font-normal">
-                <a className="inline-block px-2 py-1 text-sm text-black underline hover:underline-offset-2 hover:bg-[#C8B6FF] hover:rounded-full" href="#features">Projects</a>
-                <a className="inline-block px-2 py-1 text-sm text-black underline hover:underline-offset-2 hover:bg-[#C8B6FF] hover:rounded-full" href="#testimonials">Academic Work</a>
-                <a className="inline-block px-2 py-1 text-sm text-black underline hover:underline-offset-2 hover:bg-[#C8B6FF] hover:rounded-full" href="#pricing">Blog</a>
-              </div>
-            </div>
-          </nav>
-        </div>
-      </header>
-    </div>
-
-    <div className="flex flex-col gap-2 items-center">
-      <Image src="./headshot.jpg" alt="Head shot of Gregory Knapp, standing in front of a white background" width={150} height={150} />
-      <ol className="list-disc">
-        <li>Data Science projects</li>
-        <li>Web Dev Projects</li>
-        <li>AI/ML Work</li>
-      </ol>
-      <div className="flex flex-row gap-8 py-5">
-        <button type="button" className="text-white bg-[#C8B6FF] hover:bg-[#FFD6FF] focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-[#C8B6FF] dark:hover:bg-[#FFD6FF] dark:focus:ring-blue-800">Current Projects</button>
-        <button type="button" className="text-white bg-[#C8B6FF] hover:bg-[#FFD6FF] focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-[#C8B6FF] dark:hover:bg-[#FFD6FF] dark:focus:ring-blue-800">Completed Projects</button>
+    <div className="flex flex-col relative gap-48">
+      <NavBar />
+      <div>
+        <Cover />
       </div>
     </div>
-  </div>
   );
 }
---
+++ src/components/cover.tsx
@@ -0,0 +1,39 @@
+export default function Cover() {
+    return (
+      <div className="mx-auto w-full max-w-3xl gap-y-2">
+        <div className=" flex justify-start gap-x-10">
+          <div>
+            <span className="relative flex shrink-0 overflow-hidden w-[200px] h-[300px] rounded-[24px]">
+              <img
+                className="aspect-square h-full w-full"
+                alt="Gregory Knapp headshot"
+                src="./headshot.jpg"
+              />
+            </span>
+          </div>
+          <div className="pt-10 flex flex-col gap-y-4 text-center">
+            <div className="flex">
+              <span className="inline-block text-6xl font-bold">
+                Gregory Knapp
+              </span>
+            </div>
+            <div className="flex">
+                <p className="inline-block text-2xl w-[100%]">
+                📊 Data Science Masters Student 🎓
+              </p>
+            </div>
+            <div className="flex">
+                <p className="inline-block text-2xl w-[100%]">
+                💨 Distance Runner in Training 🏃🏻
+              </p>
+            </div>
+            <div className="flex">
+                <p className="inline-block text-2xl w-[100%]">
+                👨🏻‍💻 Problem solver with code 💻
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
