4debe7649f348602a97ce5f4820fbb839cd487dc
gknappattack <gregory.c.knapp@gmail.com>
2025-09-29 14:38:22 -0400

    Make tab page interactable with filler content.

--- src/components/tabpage.tsx
+++ src/components/tabpage.tsx
@@ -1,23 +1,52 @@
+"use client";
+
+import { useState } from "react";
+
 export default function TabPage() {
+    const [activeTab, setActiveTab] = useState("tab1")
+
+    const tabs = [
+      {id: "tab1", label:"Projects"},
+      {id: "tab2", label:"Academic Work"},
+      {id: "tab3", label:"About Me"}
+    ];
+
+    const tabContent = {
+      tab1: (
+        <div>
+          <h2 className="mb-4 text-2xl font-bold">Projects Tab</h2>
+        </div>
+      ),
+      tab2: (
+        <div>
+          <h2 className="mb-4 text-2xl font-bold">Academic Work Tab</h2>
+        </div>
+      ),
+      tab3: (
+        <div>
+          <h2 className="mb-4 text-2xl font-bold">About Me Tab</h2>
+        </div>
+      )
+    };
+
+
     return (
       <div className="p-20 justify-around">
         <div className="grid grid-cols-3 gap-5">
-          <button className="text-white p-4 rounded bg-indigo-500 shadow-md">
-            Projects
-          </button>
-          <button className="p-4 rounded bg-white text-indigo-500 shadow-md">
-            Academic Work
-          </button>
-          <button className="p-4 rounded bg-white text-indigo-500 shadow-md">
-            About Me
-          </button>
+          {tabs.map((tab) => (
+            <button key={tab.id}
+            className={`px-4 py-2 font-semibold ${
+              activeTab == tab.id ? "text-white p-4 rounded bg-indigo-500 shadow-md"
+              : "p-4 rounded bg-white text-indigo-500 shadow-md"
+              }`}
+              onClick={() => setActiveTab(tab.id)}
+            >
+              {tab.label}
+            </button>
+          ))}
         </div>
-        <div className="shadow-xl border border-gray-100 font-light p-8 rounded text-gray-500 bg-white mt-6">
-          Raw denim you probably havent heard of them jean shorts Austin.
-          Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache
-          cliche tempor, williamsburg carles vegan helvetica. Reprehenderit
-          butcher retro keffiyeh dreamcatcher synth.
-        </div>
+        <div>{tabContent[activeTab as keyof typeof tabContent]}</div>
+
       </div>
     );
 }
